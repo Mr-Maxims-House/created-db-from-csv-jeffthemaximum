e5edd4e8aee22e5f31cb5f2150cbef393fc5e37d
Daniel Rodriguez <daniel.analyst@gmail.com>
2016-04-25 12:51:53 -0400

    SQL Schema

--- createdb.py
+++ createdb.py
@@ -4,16 +4,19 @@ db = sqlite3.connect('fitness')
 
 cursor = db.cursor()
 
-cursor.execute('''
+cursor.execute(
+  '''
   CREATE TABLE gyms (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     city TEXT,
     rate INTEGER
   );
-''')
+  '''
+)
 
-cursor.execute('''
+cursor.execute(
+  '''
   CREATE TABLE members(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     gym_id INTEGER,
@@ -22,7 +25,8 @@ cursor.execute('''
     age INTEGER,
     FOREIGN KEY(gym_id) REFERENCES gyms(id)
   );
-''')
+  '''
+)
 
 
 db.commit()
