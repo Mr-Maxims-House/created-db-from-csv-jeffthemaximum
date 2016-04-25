e89a976bcd33374791f5ddbe04afa2d855d1e671
Daniel Rodriguez <daniel.analyst@gmail.com>
2016-04-25 12:50:21 -0400

    SQL Schema

--- exercises/create_db_from_csv/sql.py
+++ exercises/create_db_from_csv/sql.py
@@ -3,3 +3,20 @@ import csv
 conn = sqlite3.connect('csvdata.db')
 
 c = conn.cursor()
+
+c.execute(
+	'''
+	CREATE TABLE users (id INTEGER PRIMARY KEY,
+	name TEXT, email TEXT, country TEXT)
+	'''
+	)
+
+c.execute(
+	'''
+	CREATE TABLE phone_numbers (cellphone TEXT, homephone TEXT, 
+	workphone TEXT, FOREIGN KEY (name_id) REFERENCES users(id))
+	'''
+	)
+
+c.commit()
+
